f3513f461152adcd214932ccdd9a99af44d4771e
Alaa Elgazwy <ai6949347@gmail.com>
2025-11-22 00:20:14 +0200

    fixed templates path

--- myapp/views.py
+++ myapp/views.py
@@ -3,4 +3,4 @@ from django.http import HttpResponse
 # Create your views here.
 
 def mini_project(request):
-    return render(request, '/home/alaa/mini_DevOps_project_kodekloud/myapp/templates/index.html')
+    return render(request, 'index.html')
